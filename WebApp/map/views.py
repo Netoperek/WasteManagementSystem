d8b26928efcf077e90637f316a7ee0146dfdd683
Piotr Kala <Piotr.Kala@pega.com>
2015-01-07 20:53:19 +0100

    City with many words can be found now

--- WebApp/map/views.py
+++ WebApp/map/views.py
@@ -3,47 +3,78 @@ from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from points.models import Point
 from routes.models import Route
 from address.models import Address
-# Create your views here.
 
 def newRoute(request):
-	return render_to_response("newRoute.html",
-								locals(),
-								context_instance=RequestContext(request))
+    return render_to_response("newRoute.html",
+                                locals(),
+                                context_instance=RequestContext(request))
 def getLatitude(point):
-	return float(point.split('#')[0])
+    print "getLatitude"
+    print float(point.split('#')[0])
+    return float(point.split('#')[0])
 
 def getLongitude(point):
-	return float(point.split('#')[1])
+    print "getLongitude"
+    print float(point.split('#')[1])
+    return float(point.split('#')[1])
 
 def getAddress(point):
-	return point.split('#')[2]
+    print "getAddress"
+    print point.split('#')[2]
+    return point.split('#')[2]
 
 def getStreet(address):
-	return address.split(' ')[1]
+    print "getStreet"
+    i = getNumberPos(address)
+    split_addr = address[:i]
+    result = " ".join(split_addr)
+    print result
+    return result
+
+def getNumberPos(address):
+    i = 0
+    while i < len(address):
+        if address[i].replace(",","").isdigit():
+            break
+        i = i + 1
+
+    return i
+
 
 def getNumber(address):
-	return address.split(' ')[2][:-1]
+    print "getNumber"
+    i = getNumberPos(address)
+    print address[i].replace(",","")
+    return address[i].replace(",","")
 
 def getCode(address):
-	return address.split(' ')[3][:-1]
+    print "getCode"
+    i = getNumberPos(address) + 1
+    print address[i].replace(",","")
+    return address[i].replace(",","")
 
 def getCity(address):
-	return address.split(' ')[4][:-1]
+    print "getCity"
+    i = getNumberPos(address) + 2
+    print address[i].replace(",","")
+    return address[i].replace(",","")
 
 def saveRoute(request):
-	points = []
-	if request.POST.has_key('points'):
-		points = request.POST.getlist('points')
-		routeName =  request.POST.get('routeName')
-		route = Route(name = routeName)
-		route.save()
+    points = []
+    if request.POST.has_key('points'):
+        points = request.POST.getlist('points')
+        routeName =  request.POST.get('routeName')
+        route = Route(name = routeName)
+        route.save()
 
-		for point in points:
-			address = getAddress(point)
-		 	saveAddress = Address(street = getStreet(address), number = getNumber(address), postCode = getCode(address), city = getCity(address))
-		 	saveAddress.save()
-		 	savePoint = Point(route = route, address = saveAddress, longitude = getLongitude(point), latitude = getLatitude(point))
-		 	savePoint.save()
-		return HttpResponse('Trasa zapisana')
+        for point in points:
+            print "KURWA TU"
+            print point
+            address = getAddress(point).split(' ')[1:]
+            saveAddress = Address(street = getStreet(address), number = getNumber(address), postCode = getCode(address), city = getCity(address))
+            saveAddress.save()
+            savePoint = Point(route = route, address = saveAddress, longitude = getLongitude(point), latitude = getLatitude(point))
+            savePoint.save()
+        return HttpResponse('Trasa zapisana')
 
-	return HttpResponse('Wybierz conajmniej jeden punkt')
+    return HttpResponse('Wybierz conajmniej jeden punkt')
